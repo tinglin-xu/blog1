52023f0ee7070a21454362a785bbdc2d9020af58
xutinglin <1131180370@qq.com>
2018-10-27 18:49:23 +0800

    add index

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'home\IndexController@index');
+
+Route::group(['middleware' => ['web'], 'prefix'=>'home', 'namespace'=>'Home'], function () {
+
+    //首页
+    Route::get('index', 'IndexController@index');
 });
