a51ae4ebf38f423e1812e6badd268396cf108eba
xutinglin <1131180370@qq.com>
2018-10-27 18:56:48 +0800

    change web-route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', 'home\IndexController@index');
+Route::get('/', 'Home\IndexController@index');
 
 Route::group(['middleware' => ['web'], 'prefix'=>'home', 'namespace'=>'Home'], function () {
 
